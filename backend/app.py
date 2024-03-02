f51aee1fb0712594923440c19dda450b90565394
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2024-03-02 10:48:15 -0800

    Modified API

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def index():
-    return 'Hello World!'
+    return jsonify('Hello World!')
 
 if __name__ == '__main__':
     app.run(port=5328)
