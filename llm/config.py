5751f1f347c2784ee14749d0a02b056427c2ac8e
Keigo <keigol@students.cs.ubc.ca>
2024-03-02 14:59:16 -0800

    updates

--- llm/config.py
+++ llm/config.py
@@ -23,3 +23,18 @@ def init():
     )
 
     return boto3_bedrock
+
+def get_llm(boto3_bedrock):
+    inference_modifier = {'max_tokens_to_sample':4096, 
+                      "temperature":0.5,
+                      "top_k":250,
+                      "top_p":1,
+                      "stop_sequences": ["\n\nHuman"]
+                     }
+
+    textgen_llm = Bedrock(model_id = "anthropic.claude-v2",
+                        client = boto3_bedrock, 
+                        model_kwargs = inference_modifier 
+                        )
+    
+    return textgen_llm
--- llm/main.py
+++ llm/main.py
@@ -13,6 +13,7 @@ from langchain.prompts import PromptTemplate
 import config
 
 boto3_bedrock = config.init()
+# textgen_llm = config.get_llm(boto3_bedrock)
 
 inference_modifier = {'max_tokens_to_sample':4096, 
                       "temperature":0.5,
@@ -29,26 +30,21 @@ textgen_llm = Bedrock(model_id = "anthropic.claude-v2",
 
 # Create a prompt template that has multiple input variables
 multi_var_prompt = PromptTemplate(
-    input_variables=["customerServiceManager", "customerName", "feedbackFromCustomer"], 
-    template="""
-
-Human: Create an apology email from the Service Manager {customerServiceManager} to {customerName} in response to the following feedback that was received from the customer: 
-<customer_feedback>
-{feedbackFromCustomer}
-</customer_feedback>
-
-Assistant:"""
+    input_variables=["json", "csv"], 
+    template="""Given USER give the name of a person listed in QUERY who best matches the USER.
+    USER:{json}
+    QUERY:{csv}"""
 )
 
+with open('./data/user.example.json', 'r') as file:
+    json_data = json.load(file)
+
+with open('./data/Form_Questions_Responses_-_Copy_of_Form_responses_1.csv', 'r') as file:
+    csv_string = file.read()
+
 # Pass in values to the input variables
-prompt = multi_var_prompt.format(customerServiceManager="Bob", 
-                                 customerName="John Doe", 
-                                 feedbackFromCustomer="""Hello Bob,
-     I am very disappointed with the recent experience I had when I called your customer support.
-     I was expecting an immediate call back but it took three days for us to get a call back.
-     The first suggestion to fix the problem was incorrect. Ultimately the problem was fixed after three days.
-     We are very unhappy with the response provided and may consider taking our business elsewhere.
-     """
+prompt = multi_var_prompt.format(json=json_data, 
+                                 csv=csv_string
      )
 
 num_tokens = textgen_llm.get_num_tokens(prompt)
