a648bcfb5f92551ce6ec9e430e3753b4891f2080
Amy Jo <amyjojjm2001@gmail.com>
2024-03-02 12:45:03 -0800

    formpage component

--- my-friends/app/form/page.tsx
+++ my-friends/app/form/page.tsx
@@ -1,16 +1,5 @@
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import FormPage from "@/components/ui/formPage";
 
 export default function form() {
-  return (
-    <main>
-      <div className="w-3/5 m-auto">
-        <div className="mt-56 text-4xl font-semibold text-center">
-          what is your name?
-        </div>
-        <Input className="mt-8" placeholder="name" />
-      </div>
-      <Button>next</Button>
-    </main>
-  );
+  return <FormPage question="what is your name?" placeholder="name" />;
 }
--- my-friends/components/ui/formPage.tsx
+++ my-friends/components/ui/formPage.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Input } from "./input";
+import { Button } from "./button";
 
-const formPage = () => {
-  return <div>formPage</div>;
+type FormPageProps = {
+  question: string;
+  placeholder?: string;
 };
 
-export default formPage;
+const FormPage: React.FC<FormPageProps> = ({ question, placeholder }) => {
+  return (
+    <main>
+      <div className="w-3/5 m-auto">
+        <div className="mt-56 text-4xl font-semibold text-center">
+          {question}
+        </div>
+        <Input className="mt-8" placeholder={placeholder} />
+      </div>
+      <div className="w-11/12 mt-64">
+        <Button className="float-right">next</Button>
+      </div>
+    </main>
+  );
+};
+
+export default FormPage;
