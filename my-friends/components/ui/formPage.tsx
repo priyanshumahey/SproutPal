266eb7e3f0284cc99354c638b4cca5a0b3616ce6
Amy Jo <amyjojjm2001@gmail.com>
2024-03-02 13:18:09 -0800

    input clear

--- my-friends/components/ui/formPage.tsx
+++ my-friends/components/ui/formPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "./input";
 import { Button } from "./button";
 
@@ -13,18 +13,32 @@ const FormPage: React.FC<FormPageProps> = ({
   placeholder,
   setProgress,
 }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <main>
       <div className="w-3/5 m-auto">
         <div className="mt-56 text-4xl font-semibold text-center">
           {question}
         </div>
-        <Input className="mt-8" placeholder={placeholder} />
+        <Input
+          className="mt-8"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder={placeholder}
+        />
       </div>
       <div className="w-11/12 mt-64">
         <Button
           className="float-right"
-          onClick={() => setProgress?.((prev: number) => prev + 1)}
+          onClick={() => {
+            setProgress?.((prev: number) => prev + 1);
+            setInputValue("");
+          }}
         >
           next
         </Button>
