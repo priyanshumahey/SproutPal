730d0b508008282c396cfef6300ce43b6ab62887
Amy Jo <amyjojjm2001@gmail.com>
2024-03-02 15:33:20 -0800

    chat page

---
+++ my-friends/app/chat/page.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div className="m-auto w-3/5">
+      <div className="pt-32 text-3xl font-semibold text-center">
+        hi [name], what would you like to know about your pals?
+      </div>
+      <div className="pt-8 text-2xl text-center">examples:</div>
+      <div className="grid grid-cols-2">
+        <Card className="p-8 m-auto my-8">
+          who would i most likely get along with?
+        </Card>
+        <Card className="p-8 m-auto my-8">
+          who would i most likely get along with?
+        </Card>
+      </div>
+      <div className="flex mt-8">
+        <Input className="mr-4" placeholder="Type here" />
+        <Button className="px-8">go</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- my-friends/app/result/page.tsx
+++ my-friends/app/result/page.tsx
@@ -1,14 +1,15 @@
 import ResultCard from "@/components/ui/resultcard";
 
 export default function Result() {
+  const data = [{}, {}, {}];
   return (
     <div className="w-full">
       <div className="pt-24 text-3xl font-semibold text-center">
         we think you would get along with...
       </div>
-      <ResultCard />
-      <ResultCard />
-      <ResultCard />
+      {data.map((d) => (
+        <ResultCard />
+      ))}
     </div>
   );
 }
