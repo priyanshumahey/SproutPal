0718a8654a7ce4eb3e659102233c517687382fd4
Amy Jo <amyjojjm2001@gmail.com>
2024-03-02 16:19:44 -0800

    sprout img and ranking

--- my-friends/app/result/page.tsx
+++ my-friends/app/result/page.tsx
@@ -1,15 +1,20 @@
 import ResultCard from "@/components/ui/resultcard";
 
 export default function Result() {
-  const data = [{}, {}, {}];
+  const data = [1, 2, 3, 4, 5];
   return (
     <div className="w-full">
       <div className="pt-24 text-3xl font-semibold text-center">
-        we think you would get along with...
+        your top friend matches
       </div>
-      {data.map((d) => (
-        <ResultCard />
-      ))}
+      <div className="w-full grid grid-cols-1 mt-8 mb-16">
+        {data.map((d, i) => (
+          <div className="flex m-auto">
+            <div className="font-bold mr-8 font-3xl">{`${d}.`}</div>
+            <ResultCard />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
