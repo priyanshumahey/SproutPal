f4ba9158d7e2ec3409db39b1d98cc288c2f07d31
Keigo <keigol@students.cs.ubc.ca>
2024-03-02 16:47:04 -0800

    update llm

--- llm/inference.py
+++ llm/inference.py
@@ -1,13 +1,59 @@
 from langchain.prompts import PromptTemplate
 
-def bestMatch(textgen_llm, user, friends):
+def getTopK(textgen_llm, user, friends, k=5):
     # Create a prompt template that has multiple input variables
     multi_var_prompt = PromptTemplate(
     input_variables=["user", "friends"], 
-    template="""Given the user data give the name of a person listed in friends who best matches the user.
+    template="""Given the user data, rank the top 5 friends based on similarity. ONLY RETURN A LIST OF NAMES. NO OTHER TEXT.
     user:{user}
-    friends:{friends}"""
-)
+    friends:{friends}
+
+    Example output:
+    ["John", "Sarah", "Beatrice", "Bob", "Joe"]
+    """
+    )
+
+    # Pass in values to the input variables
+    prompt = multi_var_prompt.format(user=user, 
+                                    friends=friends)
+
+    num_tokens = textgen_llm.get_num_tokens(prompt)
+    print(f"Our prompt has {num_tokens} tokens")
+
+    response = textgen_llm(prompt)
+
+    return response[response.index('\n')+1:]
+
+def matchSummary(textgen_llm, user, match):
+    # Create a prompt template that has multiple input variables
+    multi_var_prompt = PromptTemplate(
+    input_variables=["user", "match"], 
+    template="""Give a friendly summary using the users name and their matched friend.
+    user:{user}
+    match:{match}
+    """
+    )
+
+    # Pass in values to the input variables
+    prompt = multi_var_prompt.format(user=user, 
+                                    match=match)
+
+    num_tokens = textgen_llm.get_num_tokens(prompt)
+    print(f"Our prompt has {num_tokens} tokens")
+
+    response = textgen_llm(prompt)
+
+    return response
+
+def bestMatchAndSummary(textgen_llm, user, friends):
+    # Create a prompt template that has multiple input variables
+    multi_var_prompt = PromptTemplate(
+    input_variables=["user", "friends"], 
+    template="""Given the user data, find the friend who best matches the user's profile. Give a friendly summary using the users name and their best match.
+    user:{user}
+    friends:{friends}
+    """
+    )
 
     # Pass in values to the input variables
     prompt = multi_var_prompt.format(user=user, 
--- llm/main.py
+++ llm/main.py
@@ -6,7 +6,7 @@ import botocore
 from os.path import join, dirname
 from dotenv import load_dotenv
 from utils import bedrock, print_ww
-
+import ast
 
 import config, inference
 
@@ -16,9 +16,23 @@ textgen_llm = config.get_llm(boto3_bedrock)
 with open('./data/user.example.json', 'r') as file:
     user = json.load(file)
 with open('./data/Form_Questions_Responses_-_Copy_of_Form_responses_1.csv', 'r') as file:
-    others = file.read()
+    friends = file.read()
 
-res = inference.bestMatch(textgen_llm, user, others)
+# this will return json of top_k list, and summary of the top match
+def do_inference(user, friends, k):
+    # res = inference.bestMatchAndSummary(textgen_llm, user, friends)
+    top_k = inference.getTopK(textgen_llm, user, friends, k=k)
 
-print_ww(res)
+    # convert to list
+    top_k = ast.literal_eval(top_k)
 
+    summary = inference.matchSummary(textgen_llm, user, top_k[0])
+
+    data = {
+        "top_k": top_k,
+        "summary": summary
+    }
+    return data
+
+res = do_inference(user, friends, 5)
+print(res)
